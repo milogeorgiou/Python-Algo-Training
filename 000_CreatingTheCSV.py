55384f06bdfdb637881b3225679b862ba5eb3eac
Milo Georgiou <67758603+milogeorgiou@users.noreply.github.com>
2020-07-16 15:18:58 +0100

    Update 000_CreatingTheCSV.py

--- 000_CreatingTheCSV.py
+++ 000_CreatingTheCSV.py
@@ -1,3 +1,8 @@
+###############################################################################################################################
+#The intention of this is to import the CSV file which I have written for the specifics of the next 12 functions
+#ENVIRONMENT: jupyter Python 3
+#Updated Date : 16/07/2020
+###############################################################################################################################
 #This reads/parses a CSV with players in a file with data on their game score and game time on each game, following their names.
 import pandas as pd
 import numpy as np
